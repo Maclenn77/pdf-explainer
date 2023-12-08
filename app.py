7eeda1a2759a8a48c6a77754f1f5f5f3a96171fc
Juan Paulo Pérez-Tejada <maclenn77@gmail.com>
2023-12-08 14:13:25 -0600

    fix: Extract pdf content (#4)
    * Change PDF extractor library to be used with HF

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 """ A simple example of Streamlit. """
 import streamlit as st
-from tika import parser
+import fitz
+
+# from tika import parser
+# from openai import OpenAI
+
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 pdf = st.file_uploader("Upload a file", type="pdf")
 
 if st.button("Extract text"):
     if pdf is not None:
-        extracted_text = parser.from_file(pdf)
-        st.write(extracted_text["content"])
+        with fitz.open(stream=pdf.read(), filetype="pdf") as doc:  # open document
+            text = chr(12).join([page.get_text() for page in doc])
+            st.write(text)
     else:
         st.write("Please upload a file of type: pdf")
