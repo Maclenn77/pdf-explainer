935bea674ca0e13554f815717ca88a7e0cdbe40d
Juan Paulo Pérez-Tejada <maclenn77@gmail.com>
2023-12-11 17:35:26 -0600

    refactor: separate app components (#17)

--- gnosis/gui_messages.py
+++ gnosis/gui_messages.py
@@ -2,6 +2,12 @@
 import streamlit as st
 
 
+def header():
+    """A header"""
+    st.title("GnosisPages")
+    st.subheader("Create your knowledge base")
+
+
 def api_message(api_key):
     """Inform if the api key is set."""
     if api_key is None:
